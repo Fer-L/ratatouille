f85a7319d67c87c133a798f06ae72cf4d8d525db
Fernanda Luna <fer1luna@outlook.com>
2020-11-28 15:25:33 -0400

    adicao do arquivo sql

--- database.sql
+++ database.sql
@@ -1,11 +1,15 @@
+CREATE DATABASE ratatouille;
+
+USE ratatouille;
+
 CREATE TABLE receitas (
   cod INT NOT NULL AUTO_INCREMENT,
   nomeDaReceita VARCHAR(60) NOT NULL,
   nomeAutor VARCHAR(20),
-  rendimentoPorcao INT,
+  rendimentoPorcao INT NOT NULL,
   categoria VARCHAR(20) NOT NULL,
-  tempo time NOT NULL,
-  passos VARCHAR(1024),
-  ingredientes VARCHAR(1024),
+  tempo float NOT NULL,
+  passos VARCHAR(1024) NOT NULL,
+  ingredientes VARCHAR(1024) NOT NULL,
   PRIMARY KEY(cod)
 );
